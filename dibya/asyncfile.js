83bf0b521979b100a6f2788b00264e698b87e3e9
Dibya Ghosh <djgvolt@gmail.com>
2015-09-18 12:06:36 -0700

    Added Dibya -sync

--- dibya/asyncfile.js
+++ dibya/asyncfile.js
@@ -1,6 +1,6 @@
-var fs = require 'fs'
-var filename = process.argv[2]
-var file = fs.readFileSync(filename)
-var text = file.toString()
-console.log(text.split("\n").length-1)
+var fs = require('fs');
+var filename = process.argv[2];
+var file = fs.readFileSync(filename);
+var text = file.toString();
+console.log(text.split("\n").length-1);
 
--- dibya/babysteps.js
+++ dibya/babysteps.js
@@ -1,5 +1,5 @@
-sum = 0
+sum = 0;
 for(i =2; i<process.argv.length;i++){
 	sum+= Number(process.argv[i]);
 }
-console.log(sum)
+console.log(sum);
